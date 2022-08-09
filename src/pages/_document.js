5ead5dc27f79c500dda5601a64bb23e386611ea4
Alan Dega <zero@cyphers.gg>
2022-08-09 09:25:02 -0500

    preloaded fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,30 +1,42 @@
-import React from 'react'
-import Document, {Html, Head, Main, NextScript} from 'next/document'
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import loader from "../loader";
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head/>
-                <head>
-                    <style>
-                        {loader}
-                    </style>
-                </head>
-                <body>
-                <div id={'globalLoader'}>
-                     <div className="loader">
-                        <div/>
-                        <div/>
-                    </div>
-                </div>
-                <Main/>
-                <NextScript/>
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <head>
+          <link
+            rel="preload"
+            href="/assets/Monument/MonumentExtended-Regular.otf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/assets/Monument/MonumentExtended-Ultrabold.otf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+          />
+          <style>{loader}</style>
+        </head>
+        <body>
+          <div id={"globalLoader"}>
+            <div className="loader">
+              <div />
+              <div />
+            </div>
+          </div>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
